c7f4a6193bb1426207fe6d1d7f0975589726abb3
yohan <icb1696@gmail.com>
2025-10-01 01:09:32 +0900

    Feat : vm_try_hand_fault 껍데기 구현

--- pintos/vm/vm.c
+++ pintos/vm/vm.c
@@ -163,8 +163,14 @@ bool vm_try_handle_fault(struct intr_frame *f UNUSED, void *addr UNUSED,
                          bool not_present UNUSED) {
   struct supplemental_page_table *spt UNUSED = &thread_current()->spt;
   struct page *page = NULL;
-  /* TODO: Validate the fault */
-  /* TODO: Your code goes here */
+  if (addr == NULL || is_kernel_vaddr(addr) || not_present) {
+    return false;
+  }
+
+  page = spt_find_page(spt, addr);
+  if (page == NULL) {
+    return false;
+  }
 
   return vm_do_claim_page(page);
 }
