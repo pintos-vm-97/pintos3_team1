fec402627f838a3b21556955bc87c6345e279ed6
yohan <icb1696@gmail.com>
2025-10-01 00:50:46 +0900

    feat : spt init 구현

--- pintos/vm/vm.c
+++ pintos/vm/vm.c
@@ -186,7 +186,9 @@ static bool vm_do_claim_page(struct page *page) {
 }
 
 /* Initialize new supplemental page table */
-void supplemental_page_table_init(struct supplemental_page_table *spt UNUSED) {}
+void supplemental_page_table_init(struct supplemental_page_table *spt UNUSED) {
+  hash_init(&spt->page_table, hash_hash_func, hash_less_func, NULL);
+}
 
 /* Copy supplemental page table from src to dst */
 bool supplemental_page_table_copy(struct supplemental_page_table *dst UNUSED,
@@ -196,4 +198,16 @@ bool supplemental_page_table_copy(struct supplemental_page_table *dst UNUSED,
 void supplemental_page_table_kill(struct supplemental_page_table *spt UNUSED) {
   /* TODO: Destroy all the supplemental_page_table hold by thread and
    * TODO: writeback all the modified contents to the storage. */
+}
+
+static uint64_t hash_hash_func(const struct hash_elem *e, void *aux) {
+  struct page *p = hash_entry(e, struct page, hash_elem);
+  return hash_bytes(&p->va, sizeof(p->va));
+}
+
+static bool hash_less_func(const struct hash_elem *a, const struct hash_elem *b,
+                           void *aux) {
+  struct page *p1 = hash_entry(a, struct page, hash_elem);
+  struct page *p2 = hash_entry(a, struct page, hash_elem);
+  return p1->va < p2->va;
 }
